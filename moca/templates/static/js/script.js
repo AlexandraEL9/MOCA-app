25e2b09f647fd49f2a29861ad63425fb3b3fbddd
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-13 17:01:41 +0000

    feat: add header navbar and side navbar and initialise in js file

---
+++ moca/templates/static/js/script.js
@@ -0,0 +1,6 @@
+//mobile side nav initialisation
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});

c32e6e5625eb6b2825afae49bc01db7d9b96e9aa
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-14 11:44:34 +0000

    add route for add category

--- moca/routes.py
+++ moca/routes.py
@@ -6,3 +6,7 @@ from moca.models import Category, Recipe
 @app.route("/")
 def home():
     return render_template("recipes.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")

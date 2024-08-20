54c48511c585a0fd47631190c3317a86b7df7dd7
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-20 15:14:44 +0000

    feat: add route for deleting a recipe

--- moca/routes.py
+++ moca/routes.py
@@ -115,3 +115,12 @@ def edit_recipe(recipe_id):
 
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
+# Delete a recipe
+@app.route("/delete_recipe/<int:recipe_id>")
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+    db.session.delete(recipe)
+    db.session.commit()
+    flash("Recipe deleted successfully!", "success")
+    return redirect(url_for("recipes"))
+

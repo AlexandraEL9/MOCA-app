f6312c2308453358d357bce83f9c1d993fa73dce
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-23 18:39:56 +0000

    feat: add function for card reveal and toggling of card front and back

--- moca/static/js/script.js
+++ moca/static/js/script.js
@@ -8,3 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 });
+
+//card actions
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners for the reveal icons
+    const revealIcons = document.querySelectorAll('.card-image .btn-floating');
+    const closeIcons = document.querySelectorAll('.card-reveal .fa-times-circle');
+    
+    revealIcons.forEach((icon) => {
+        icon.addEventListener('click', function() {
+            const card = icon.closest('.card');
+            card.classList.toggle('active');
+        });
+    });
+
+    closeIcons.forEach((icon) => {
+        icon.addEventListener('click', function() {
+            const card = icon.closest('.card');
+            card.classList.toggle('active');
+        });
+    });
+});

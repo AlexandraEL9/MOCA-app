d8a4c58ccfdce897edfb2b70e3d73a28e6d75fb9
Alexandra Lee <alexandraelee9@gmail.com>
2024-09-03 16:32:43 +0000

    update: add column to recipes table to enable users to add an image file or a url

--- moca/models.py
+++ moca/models.py
@@ -11,11 +11,11 @@ class Category(db.Model):
     def __repr__(self):
         return f'<Category {self.category_name}>'
 
-#table for recipes
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     recipe_name = db.Column(db.String(50), nullable=False)
-    image_url = db.Column(db.String(200), nullable=False)
+    image_url = db.Column(db.String(1000),nullable=True)
+    image_file = db.Column(db.String(255), nullable=True)  # New column to store the uploaded image filename
     description = db.Column(db.String(150), nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)

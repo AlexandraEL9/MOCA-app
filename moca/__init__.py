3bcdcc2cb9ca17964fbd449e246b750a2203b440
Alexandra Lee <alexandraelee9@gmail.com>
2024-09-19 13:28:22 +0000

    fix: amend order of imports to fix circular import issue

--- moca/__init__.py
+++ moca/__init__.py
@@ -1,13 +1,10 @@
 import os
-import moca.routes
-import moca.models
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from moca import routes  # noqa
+
 if os.path.exists("env.py"):
     import env  # noqa
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -15,5 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Add this line
 
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
+
+# Import routes and models at the end to avoid circular imports
+import moca.routes  # noqa
+import moca.models  # noqa

65924a5b6ff5253aa9d2811dc8294b2851011c93
Alexandra Lee <alexandraelee9@gmail.com>
2024-09-19 12:41:56 +0000

    edit: validation

--- moca/__init__.py
+++ moca/__init__.py
@@ -1,6 +1,10 @@
 import os
+import moca.routes
+import moca.models
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from moca import routes  # noqa
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -8,15 +12,8 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # Add this line
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Add this line
 
 db = SQLAlchemy(app)
 
-# Ensure this is added
-from flask_migrate import Migrate
 migrate = Migrate(app, db)
-
-from moca import routes  # noqa
-
-import moca.routes
-import moca.models
